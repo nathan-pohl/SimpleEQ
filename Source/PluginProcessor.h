6d5907011f89debbecbce0a2c086c0ad2ae7d068
Nathan Pohl <nathan.a.pohl@gmail.com>
2021-10-16 16:23:11 -0600

    Added basic dsp. Added a default for AudioPluginHost

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -57,6 +57,16 @@ public:
     juce::AudioProcessorValueTreeState apvts{ *this, nullptr, "Parameters", createParameterLayout()};
 
 private:
+    // Use a bunch of aliases here to shorten down all the JUCE namespaces
+    using Filter = juce::dsp::IIR::Filter<float>;
+    // each fiter type has a response type of 12db/Oct. If we want a filter to do 48db/Oct, then we need to chain together 4 filters.
+    // This will require using a dsp processor chain to process all the audio as if it were a 48db/Oct filter.
+    using CutFilter = juce::dsp::ProcessorChain<Filter, Filter, Filter, Filter>;
+    // First CutFilter is lowpass, mid filter is the bandpass (peak filter), last filter is the highpass
+    using MonoChain = juce::dsp::ProcessorChain<CutFilter, Filter, CutFilter>;
+    // must create two chains, one for left and right audio for full stereo
+    MonoChain leftChain, rightChain;
+
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SimpleEQAudioProcessor)
 };
